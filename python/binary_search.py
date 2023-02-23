e42740d6448f00c1fc2674de2b94b89cc8365d25
nfrankeln <ntnyfranky@gmail.com>
2023-02-22 19:15:48 -0600

    added iterative method

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,22 @@
-def binary_search():
+# this function shoule return an index if found and -1 if not found
+def Iter_binary_search(arr,element_to_find):
+    start_index = 0
+    end_index = len(arr)-1
+    
+
+    while start_index <= end_index:
+        
+
+        middle_index = (start_index+end_index)//2
+     
+        if element_to_find == arr[middle_index]:
+            return middle_index
+            
+        elif element_to_find < arr[middle_index]:
+            end_index = middle_index - 1
+
+        elif element_to_find > arr[middle_index]:
+            start_index = middle_index + 1
+    return -1 
     pass
+
