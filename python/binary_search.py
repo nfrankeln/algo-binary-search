51a927e72a36da6bf068ac0158798ebf65834e41
nfrankeln <ntnyfranky@gmail.com>
2023-02-22 19:50:06 -0600

    added recursive method

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,5 +1,6 @@
+import random
 # this function shoule return an index if found and -1 if not found
-def Iter_binary_search(arr,element_to_find):
+def iter_binary_search(arr,element_to_find):
     start_index = 0
     end_index = len(arr)-1
     
@@ -19,4 +20,24 @@ def Iter_binary_search(arr,element_to_find):
             start_index = middle_index + 1
     return -1 
     pass
+
+def recursive_binary_search(arr,element_to_find, start_index = 0, end_index = None):
+    if end_index is None:
+        end_index = len(arr) - 1 
+
+    if start_index > end_index:
+        return -1
+
+    middle_index = (start_index + end_index)//2
+
+    if element_to_find == arr[middle_index]:
+            return middle_index
+    
+    elif element_to_find < arr[middle_index]:
+        end_index = middle_index - 1
+        return recursive_binary_search(arr,element_to_find, start_index, end_index)
+    
+    elif element_to_find > arr[middle_index]:
+        start_index = middle_index + 1
+        return recursive_binary_search(arr,element_to_find, start_index, end_index)
 
